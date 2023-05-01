457409bc9fb858f9a8155973763d199258c6628b
quiloos39 <hello@necdet.xyz>
2023-05-01 20:34:37 +0300

    feat(backend): add incident collection

---
+++ packages/backend/src/api/incident/controllers/incident.ts
@@ -0,0 +1,7 @@
+/**
+ * incident controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::incident.incident');
---
+++ packages/backend/src/api/incident/routes/incident.ts
@@ -0,0 +1,7 @@
+/**
+ * incident router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::incident.incident');
