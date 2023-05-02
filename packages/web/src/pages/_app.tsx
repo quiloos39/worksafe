a4cfa66341fedb1c6b967e7bf5da17802d6305ad
quiloos39 <hello@necdet.xyz>
2023-05-02 14:27:11 +0300

    chore(web): export queryClient

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 config.autoAddCss = false;
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
