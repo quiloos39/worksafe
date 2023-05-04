0d81cccbf77515b7aa6663915fc32a6adcb5d6f3
quiloos39 <hello@necdet.xyz>
2023-05-05 01:45:10 +0300

    chore(mobile): disable manually loading icons

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,7 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { loadAsync } from "expo-font";
 import { NativeBaseProvider } from "native-base";
-import { useEffect } from "react";
 import "react-native-gesture-handler";
 import { AuthContextProvider } from "./src/components/AuthContextProvider/AuthContextProvider";
 import { Navigation } from "./src/navigators";
@@ -9,12 +7,12 @@ import { Navigation } from "./src/navigators";
 export const queryClient = new QueryClient();
 
 export default function App() {
-  useEffect(() => {
-    async function loadFont() {
-      await loadAsync("../../node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf");
-    }
-    loadFont();
-  }, []);
+  // useEffect(() => {
+  //   async function loadFont() {
+  //     await loadAsync("../../node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf");
+  //   }
+  //   loadFont();
+  // }, []);
 
   return (
     <AuthContextProvider>
