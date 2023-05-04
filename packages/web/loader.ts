02e80eb3fea7567d36aca2345ec518a6103a0602
quiloos39 <hello@necdet.xyz>
2023-05-04 23:47:29 +0300

    patch(web): fix invariece

--- packages/web/loader.ts
+++ packages/web/loader.ts
@@ -1,4 +1,4 @@
-const imageUrl = process.env.IMAGE_URL;
+const imageUrl = process.env.NEXT_PUBLIC_IMAGE_URL;
 
 export default function myImageLoader({ src, width, quality }: { src: string; width: number; quality: number }) {
   return src.includes("/uploads") ? `${imageUrl}${src}` : src;
