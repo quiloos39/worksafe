5a98a9109ffcaffd8b0f59d7ab48faa8ceb18f8d
quiloos39 <hello@necdet.xyz>
2023-05-04 23:43:04 +0300

    patch(web): fix invariant image

--- packages/web/loader.ts
+++ packages/web/loader.ts
@@ -1,5 +1,5 @@
 const imageUrl = process.env.IMAGE_URL;
 
 export default function myImageLoader({ src, width, quality }: { src: string; width: number; quality: number }) {
-  return src.startsWith("/uploads") ? `${imageUrl}${src}` : src;
+  return src.includes("/uploads") ? `${imageUrl}${src}` : src;
 }
